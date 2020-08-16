b47fc2a99a7d11d0e0d0171ca0efd806209a087e
Rafflesiaceae <rafflesiaceae.plant@gmail.com>
2020-08-16 12:58:10 +0200

    add support for commands

--- main.go
+++ main.go
@@ -3,24 +3,46 @@ package main
 import (
 	"bytes"
 	"flag"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"os/user"
+	pathlib "path"
 	"strings"
 
 	yaml "gopkg.in/yaml.v2"
 )
 
+func copyToClipboard(val string) {
+	cmd := exec.Command("xclip", "-selection", "clipboard")
+
+	cmd.Stdin = strings.NewReader(val)
+
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	var (
-		url  string
-		path string
+	var ( // flags
+		url   string
+		path  string
+		bench bool
 	)
 
 	flag.StringVar(&url, "url", "", "url to yaml")
 	flag.StringVar(&path, "path", "", "path to yaml")
+	flag.BoolVar(&bench, "bench", false, "print time spent to stderr")
 	flag.Parse()
+
+	// var start Time
+
+	// if bench {
+	// 	start = time.Now()
+	// }
 
 	var rawContents []byte
 	if path != "" { // read from file
@@ -46,7 +68,7 @@ func main() {
 
 	}
 
-	contents := make(map[string]string)
+	contents := make(map[string]interface{})
 	{ // parse yaml
 		if err := yaml.Unmarshal(rawContents, &contents); err != nil {
 			panic(err)
@@ -64,9 +86,26 @@ func main() {
 		}
 	}
 
+	// if bench {
+	// 	since
+	// }
+
 	var key string
 	{ // fzf - prompt for key
-		cmd := exec.Command("fzf")
+		var homeDir string
+		{ // get homeDir
+			user, err := user.Current()
+			if err != nil {
+				panic(err)
+			}
+			homeDir = user.HomeDir
+		}
+		cmd := exec.Command("fzf",
+			"--no-info",
+			fmt.Sprintf("--history=%s",
+				pathlib.Join(homeDir, ".history_ezautocompl")),
+			// "--expect=tab",
+		)
 
 		var stdoutBuf bytes.Buffer
 
@@ -82,13 +121,65 @@ func main() {
 		key = strings.TrimSpace(key)
 	}
 
-	{ // copy to clipboard
-		cmd := exec.Command("xclip", "-selection", "clipboard")
+	val := contents[key]
+	switch t := val.(type) {
+	case map[interface{}]interface{}:
+		{ // run cmd
+			ival := val.(map[interface{}]interface{})
+			val := make(map[string]interface{})
+			for k, v := range ival {
+				k2 := k.(string)
+				val[k2] = v
+			}
+			// fmt.Printf("LOL%v\n", val)
 
-		cmd.Stdin = strings.NewReader(contents[key])
+			var cmds []string
+			if s, ok := val["cmd"].(string); ok {
+				// cmds = make([]string, 1)
+				cmds = append(cmds, s)
+			} else if rs, ok := val["cmd"].([]interface{}); ok {
+				for _, v := range rs {
+					cmds = append(cmds, v.(string))
+				}
+			} else {
+				log.Fatalf("cmds: must either be of type string or []string, is %T\n", val["cmd"])
+			}
 
-		if err := cmd.Run(); err != nil {
-			panic(err)
+			var stdin string
+			if val, ok := val["stdin"]; ok {
+				stdin = val.(string)
+			}
+
+			{ // run command
+				cmd := exec.Command(cmds[0], cmds[1:]...)
+
+				var stdoutBuf, stderrBuf bytes.Buffer
+				cmd.Stdout = &stdoutBuf
+				cmd.Stderr = &stderrBuf
+
+				if stdin != "" {
+					cmd.Stdin = strings.NewReader(stdin)
+				}
+
+				err := cmd.Run()
+				if err != nil {
+					if exitError, ok := err.(*exec.ExitError); ok {
+						log.Fatalf("cmd %v returned %d with error:\nstdout: %s\nstderr: %s",
+							cmds,
+							exitError.ExitCode(),
+							string(stdoutBuf.Bytes()),
+							string(stderrBuf.Bytes()))
+					}
+				}
+
+				outStr, _ := string(stdoutBuf.Bytes()), string(stderrBuf.Bytes())
+				copyToClipboard(outStr)
+			}
 		}
+	case string:
+		copyToClipboard(val.(string))
+	default:
+		fmt.Printf("unexpected type %T\n", t)
 	}
+
 }
